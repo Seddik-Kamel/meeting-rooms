d4e91961a953629a2829a81d39a5875c3ba954ab
KS <seddikkamel1@gmail.com>
2021-08-27 14:48:21 +0200

    Add BaseActivity

--- app/src/main/java/com/example/meetingrooms/MainActivity.java
+++ app/src/main/java/com/example/meetingrooms/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.meetingrooms;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.meetingrooms.baseActivity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/meetingrooms/baseActivity/BaseActivity.java
@@ -0,0 +1,6 @@
+package com.example.meetingrooms.baseActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+}
