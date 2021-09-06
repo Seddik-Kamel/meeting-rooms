77972678d24570f555f81691655dfac3d4fbf961
KS <seddikkamel1@gmail.com>
2021-09-06 10:57:20 +0200

    Add Meeting

--- app/src/main/java/com/example/meetingrooms/fakeData/FakeDataGenerator.java
+++ app/src/main/java/com/example/meetingrooms/fakeData/FakeDataGenerator.java
@@ -1,6 +1,7 @@
 package com.example.meetingrooms.fakeData;
 
 import com.example.meetingrooms.model.Meeting;
+import com.example.meetingrooms.model.Participant;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,18 +11,59 @@ public class FakeDataGenerator {
     private static final List<String> LIST_PARTICIPANTS = Arrays.asList(
             "lolo@gmailcom","laurent@gmailcom","Charles@gmailcom","Marc@gmailcom","Eric@gmailcom","Idir@gmailcom");
 
+    private static final Participant participant = new Participant("Seddik", "Kamel", 36,"seddikkamel1@gmail.com" );
 
-    public static List<Meeting> DUMMY_MEETING = Arrays.asList(
-            new Meeting(1, "8h00", "Ouranos", "Le pair programming",LIST_PARTICIPANTS ),
-            new Meeting(2, "8h00", "Apollon", "TDD",LIST_PARTICIPANTS ),
-            new Meeting(3, "8h00", "Arès", "Coroutine Kotlin",LIST_PARTICIPANTS ),
-            new Meeting(4, "8h00", "Athena", "BDD",LIST_PARTICIPANTS ),
-            new Meeting(5, "8h00", "Demeter", "DEV OPS",LIST_PARTICIPANTS ),
-            new Meeting(6, "8h00", "Hera", "Le pair programming",LIST_PARTICIPANTS ),
-            new Meeting(7, "8h00", "Hermes", "Le pair programming",LIST_PARTICIPANTS ),
-            new Meeting(8, "8h00", "Hestia", "Le pair programming",LIST_PARTICIPANTS ),
-            new Meeting(9, "8h00", "Rhea", "Le pair programming",LIST_PARTICIPANTS ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",LIST_PARTICIPANTS )
+    public static final List<Meeting> DUMMY_MEETING = Arrays.asList(
+            new Meeting(1, "8h00", "Ouranos", "Le pair programming",participant ),
+            new Meeting(2, "8h00", "Apollon", "TDD",participant ),
+            new Meeting(3, "8h00", "Arès", "Coroutine Kotlin",participant ),
+            new Meeting(4, "8h00", "Athena", "BDD",participant ),
+            new Meeting(5, "8h00", "Demeter", "DEV OPS",participant ),
+            new Meeting(6, "8h00", "Hera", "Le pair programming",participant ),
+            new Meeting(7, "8h00", "Hermes", "Le pair programming",participant ),
+            new Meeting(8, "8h00", "Hestia", "Le pair programming",participant ),
+            new Meeting(9, "8h00", "Rhea", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant )
     );
 
 }
