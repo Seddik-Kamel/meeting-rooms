da42e129de38b93386001ddb72d61b819eacf373
KS <seddikkamel1@gmail.com>
2021-09-13 17:53:31 +0200

    Add Hilt (DI)

---
+++ app/src/main/java/com/example/meetingrooms/application/MeetingApplication.java
@@ -0,0 +1,9 @@
+package com.example.meetingrooms.application;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MeetingApplication extends Application {
+}
