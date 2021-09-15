74864c31ff4bc474093862cd6d3b7a0c4abd0d3d
KS <seddikkamel1@gmail.com>
2021-09-15 21:24:24 +0200

    Add Circle for image item

--- app/src/main/java/com/example/meetingrooms/fakeData/FakeDataGenerator.java
+++ app/src/main/java/com/example/meetingrooms/fakeData/FakeDataGenerator.java
@@ -1,5 +1,6 @@
 package com.example.meetingrooms.fakeData;
 
+import com.example.meetingrooms.R;
 import com.example.meetingrooms.model.Meeting;
 import com.example.meetingrooms.model.Participant;
 
@@ -8,62 +9,81 @@ import java.util.List;
 
 public class FakeDataGenerator {
 
-    private static final List<String> LIST_PARTICIPANTS = Arrays.asList(
-            "lolo@gmailcom","laurent@gmailcom","Charles@gmailcom","Marc@gmailcom","Eric@gmailcom","Idir@gmailcom");
+    public static final int OURANOS_COLOR = R.drawable.pink_circle;
+    public static final int APOLLON_COLOR = R.drawable.blue_circle;
+    public static final int ARES_COLOR = R.drawable.red_circle;
+    public static final int ATHENA_COLOR = R.drawable.yellow_circle;
+    public static final int DEMETER_COLOR = R.drawable.green_circle;
+    public static final int HERA_COLOR = R.drawable.black_circle;
+    public static final int HERMES_COLOR = R.drawable.brown_circle;
+    public static final int HESTIA_COLOR = R.drawable.purple_circle;
+    public static final int RHEA_COLOR = R.drawable.orange_circle;
+    public static final int POSEIDON_COLOR = R.drawable.cyan_circle;
 
-    private static final Participant participant = new Participant("Seddik", "Kamel", 36,"seddikkamel1@gmail.com" );
+
+    private static final List<Participant> PARTICIPANT_LIST = Arrays.asList(
+            new Participant("Seddik", "Kamel", 36, "seddikkamel1@gmail.com"),
+            new Participant("Seddik", "Kamel", 36, "seddikkamel1@gmail.com"),
+            new Participant("Seddik", "Kamel", 36, "seddikkamel1@gmail.com"),
+            new Participant("Seddik", "Kamel", 36, "seddikkamel1@gmail.com"),
+            new Participant("Seddik", "Kamel", 36, "seddikkamel1@gmail.com"),
+            new Participant("Seddik", "Kamel", 36, "seddikkamel1@gmail.com"),
+            new Participant("Seddik", "Kamel", 36, "seddikkamel1@gmail.com")
+    );
+
 
     public static final List<Meeting> DUMMY_MEETING = Arrays.asList(
-            new Meeting(1, "8h00", "Ouranos", "Le pair programming",participant ),
-            new Meeting(2, "8h00", "Apollon", "TDD",participant ),
-            new Meeting(3, "8h00", "Arès", "Coroutine Kotlin",participant ),
-            new Meeting(4, "8h00", "Athena", "BDD",participant ),
-            new Meeting(5, "8h00", "Demeter", "DEV OPS",participant ),
-            new Meeting(6, "8h00", "Hera", "Le pair programming",participant ),
-            new Meeting(7, "8h00", "Hermes", "Le pair programming",participant ),
-            new Meeting(8, "8h00", "Hestia", "Le pair programming",participant ),
-            new Meeting(9, "8h00", "Rhea", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant ),
-            new Meeting(10, "8h00", "Poseidon", "Le pair programming",participant )
+            new Meeting(1, "8h00", "Ouranos", "Le pair programming", PARTICIPANT_LIST, OURANOS_COLOR),
+            new Meeting(2, "8h00", "Apollon", "TDD", PARTICIPANT_LIST, APOLLON_COLOR),
+            new Meeting(3, "8h00", "Arès", "Coroutine Kotlin", PARTICIPANT_LIST, ARES_COLOR),
+            new Meeting(4, "8h00", "Athena", "BDD", PARTICIPANT_LIST, ATHENA_COLOR),
+            new Meeting(5, "8h00", "Demeter", "DEV OPS", PARTICIPANT_LIST, DEMETER_COLOR),
+            new Meeting(6, "8h00", "Hera", "Le pair programming", PARTICIPANT_LIST, HERA_COLOR),
+            new Meeting(7, "8h00", "Hermes", "Le pair programming", PARTICIPANT_LIST, HERMES_COLOR),
+            new Meeting(8, "8h00", "Hestia", "Le pair programming", PARTICIPANT_LIST, HESTIA_COLOR),
+            new Meeting(9, "8h00", "Rhea", "Le pair programming", PARTICIPANT_LIST, RHEA_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR ),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR),
+            new Meeting(10, "8h00", "Poseidon", "Le pair programming", PARTICIPANT_LIST, POSEIDON_COLOR)
     );
 
 }
